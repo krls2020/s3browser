1c84446ed2e443be2bdc2d81218b139e15d09ec8
krls2020 <mail@karelhemza.cz>
2024-10-01 10:52:44 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\S3Controller;
+use App\Http\Controllers\S3controller;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [S3Controller::class, 'index'])->name('index');
-Route::get('/upload-test-file', [S3Controller::class, 'uploadTestFile']);
+Route::get('/', [S3controller::class, 'index'])->name('index');
+Route::get('/upload-test-file', [S3controller::class, 'uploadTestFile']);
